452cab6bae38405e6ea15b8dc375a7a35bd1c078
User <User@DESKTOP-3O6KIK7>
2023-10-25 17:29:44 +0900

    - DB Connection Pooling - Oracle 8i, 9i & 10g (https://tomcat.apache.org/tomcat-9.0-doc/jndi-datasource-examples-howto.html)

--- src/main/java/com/shinhan/util/DBUtil.java
+++ src/main/java/com/shinhan/util/DBUtil.java
@@ -6,10 +6,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class DBUtil {
 	
-	//DB연결
+	//연결된 DB Connection Pooling에서 얻기.... 연결 속도가 빠르다.
 	public static Connection getConnection() {
+		Connection conn =null;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	//DB연결
+	public static Connection getConnection2() {
 		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 		String userid="hr", password="hr";
 		Connection conn = null;	//연결
